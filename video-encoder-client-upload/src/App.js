771dabca405a3ac5d6de8d2f29cee353c0131abf
Rodolfo Silva <rosilva@fitec.org.br>
2018-07-11 17:56:04 -0300

    SAMBA-CHALLENGE: Styling client main App | Dependencies update

--- video-encoder-client-upload/src/App.js
+++ video-encoder-client-upload/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+
+import { history } from "./history";
+import Home from './pages/home';
+import Header from './components/header';
+import Videos from './pages/videos';
+import ItemVideo from './pages/item-video';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Samba Challenge Video Encoder</h1>
-      </div>
+        <div className={'samba-app-layout'}>
+            <Header />
+            <Router history={history}>
+                <Switch>
+                    <Route path={'/'} exact ={true} component={ Home } />
+                    <Route path={'/videos'} component={ Videos } />
+                    <Route path={'/videos/:id'} component={ ItemVideo } />
+                </Switch>
+            </Router>
+        </div>
     );
   }
 }
