74d65f7ddb5a0432e1156e777acd6b86b70b31da
Rodolfo Silva <rosilva@fitec.org.br>
2018-07-16 18:22:18 -0300

    SAMBA-CHALLENGER: Videos Component Tests [WIP]

--- video-encoder-client-upload/src/components/videos/Videos.test.js
+++ video-encoder-client-upload/src/components/videos/Videos.test.js
@@ -3,13 +3,20 @@ import fetchMock from 'fetch-mock';
 import Enzymer from 'enzyme';
 import { configure, shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import jsdom from 'jsdom';
+import sinon from 'sinon';
 
 import Videos from './';
 import mockVideos from '../../mocks/videos-mock-obj.json';
 
+// test setup with JSDOM
+const { window } = new jsdom.JSDOM('<!doctype html><html><body></body></html>');;
+
+global.window = window;
+global.document = window.document;
 Enzymer.configure({adapter: new Adapter()});
 
-const DELAY_MS = 2000
+const DELAY_MS = 4000
 const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -47,9 +54,9 @@ class video extends Component {
         })
     }
 }
-// const doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
-// global.document = doc;
-// global.window = doc.defaultView;
+const doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
 fetchMock.get(`*`, JSON.stringify(mockVideos));
 
 describe('Videos ', () => {
@@ -62,6 +69,7 @@ describe('Videos ', () => {
                 data:[{job:{output_media_files:[{format:'mpeg4'}]}}]
             }
         }
+        wrapper = shallow(<Videos />);
     })
 
     it("always renders a div", () => {
@@ -69,21 +77,26 @@ describe('Videos ', () => {
     });
 
     it("should call API fetching Videos", () => {
-        const responseJson = fetchResponseJson(`https://samba-challenge-api.herokuapp.com/videos`)
-        expect(responseJson).not.toHaveProperty(`data`, `job`)
+        const responseJson = fetchResponseJson(`https://samba-challenge-api.herokuapp.com/videos`);
+        responseJson.then((data) => {
+            expect(data).not.toHaveProperty(`data`, `job`)
+        })
     });
 
     it(`using promises and check if has mpeg4 format`, () => {
-        expect.assertions(1)
-        const wrapper = shallow(<Videos />);
         return fetchResponseJson(`https://samba-challenge-api.herokuapp.com/videos`).then(
             (responseJson) => { expect(responseJson).toHaveProperty(`format`,`mpeg4`) })
     })
 
     it("on a React component that loads data into state in componentDidMount", () => {
-       const wrapper = shallow(<Videos />);
-       wrapper.instance().componentDidMount();
-       sleep(DELAY_MS + 1000);
-       expect(wrapper.state(`videos`)).toHaveProperty(`data`, []);
+        sinon.spy(Videos.prototype, 'componentDidMount');
+        const wrapper = mount(<Videos />);
+        expect(Videos.prototype.componentDidMount.calledOnce).to.equal(true);
     })
+
+    it("should have proper props", () => {
+        const wrapperMount = mount(<Videos videos="data" />);
+        expect(wrapperMount.props().videos).to.equal('data');
+    })
+
 })
